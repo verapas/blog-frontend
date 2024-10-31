1db6abe1d8d51b49f023dd91ea3043c4b58ec58c
PascalVeraguth <pascal.veraguth@cyrotech.ch>
2024-10-31 14:31:58 +0100

    Added mai page, updated Login Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {MainPageComponent} from './main-page/main-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'main', component: MainPageComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' } // Standard auf Login-Seite umleiten
+];
